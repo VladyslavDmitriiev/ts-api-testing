12a363e967ed57887ecfa3461244d2b2305b10ee
Vladyslav Dmitriyev <vladyslav.dmitriyev@gmail.com>
2021-11-26 18:02:39 +0200

    add pet create, update, delete

--- api/controller/PetController.ts
+++ api/controller/PetController.ts
@@ -16,4 +16,61 @@ export default class PetController {
     const response = await got(this.baseUrl + 'findByStatus', {searchParams: new URLSearchParams({status})})
     return JSON.parse(response.body)
   }
+
+  async addNew(pet: {
+    "id": number,
+    "category": {
+      "id": number,
+      "name": string
+    },
+    "name": string,
+    "photoUrls": [
+      string
+    ],
+    "tags": [
+      {
+        "id": number,
+        "name": string
+      }
+    ],
+    "status": string
+  }) {
+    const response = await got(this.baseUrl, {
+      method: 'POST',
+      json: pet
+    })
+    return JSON.parse(response.body)
+  }
+
+  async delete(id: number | string) {
+    const response = await got(this.baseUrl + id, {
+      method: 'DELETE'
+    })
+    return JSON.parse(response.body)
+  }
+
+  async update(pet: {
+    "id": number,
+    "category": {
+      "id": number,
+      "name": string
+    },
+    "name": string,
+    "photoUrls": [
+      string
+    ],
+    "tags": [
+      {
+        "id": number,
+        "name": string
+      }
+    ],
+    "status": string
+  }) {
+    const response = await got(this.baseUrl, {
+      method: 'PUT',
+      json: pet
+    })
+    return JSON.parse(response.body)
+  }
 }
--- test/example.test.ts
+++ test/example.test.ts
@@ -2,16 +2,40 @@ import { ApiClient } from '../api/ApiClient'
 
 
 describe('User can', () => {
-  it('receive pet by his id', async function () {
+  test('receive pet by his id', async function () {
     const api = new ApiClient()
-    const body = await api.pet.findById('1')
+    let body = await api.pet.findById('1')
     expect(body.id).toEqual(1)
   })
   test('receive pet by his status', async function () {
     const api = new ApiClient()
-    const body: any[] = await api.pet.findByStatus(['sold', 'available'])
+    let body: any[] = await api.pet.findByStatus(['sold', 'available'])
     expect(body.map(pet => pet.status)).toContain('sold')
     expect(body.map(pet => pet.status)).toContain('available')
     expect(body.map(pet => pet.status)).not.toContain('pending')
+
+    body = await api.pet.findByStatus('pending')
+    expect(body.map(pet => pet.status)).toContain('pending')
+  })
+  test('create new pet', async () => {
+    const api = new ApiClient()
+    let body = api.pet.addNew({
+      "id": 0,
+      "category": {
+        "id": 0,
+        "name": "cat"
+      },
+      "name": "Leo",
+      "photoUrls": [
+        "string"
+      ],
+      "tags": [
+        {
+          "id": 0,
+          "name": "bengal"
+        }
+      ],
+      "status": "available"
+    })
   })
 })
